9647f7dda0bc8523be54c5691cb640dce17855f1
lolitaad989 <lolitaad19@gmail.com>
2023-01-16 17:54:42 +0530

    If

--- 11-if-condition.sh
+++ 11-if-condition.sh
@@ -13,8 +13,8 @@ c=$1
 
 if [ $c -eq '10' ] ; then
     echo "Value is 10"
-elif [ $c -eq '15']
+elif [ $c -eq '15'] ; then
     echo "Value is 15"
-else [ $c -eq '25']
+else [ $c -eq '25'] ; then
     echo "Value is 25"
 fi
