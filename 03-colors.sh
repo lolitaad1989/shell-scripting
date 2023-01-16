bd42d48acc7a9c83f059b8d7e12dccdccfa0e3df
lolitaad989 <lolitaad19@gmail.com>
2023-01-16 11:09:30 +0530

    colors 3 v1

--- 03-colors.sh
+++ 03-colors.sh
@@ -8,3 +8,6 @@ echo -e "\e[34m I am printing Blue Color \e[0m"
 echo -e "\e[35m I am printing Cyan Color \e[0m"
 
 echo -e "\e[41;35m I am printing Cyan Color \e[0m"
+
+echo -e "\e[41;33m Welcome to colors \e[0m "
+echo -e "\e[47;32m Avatar2 is a very good filn \e[0m "
