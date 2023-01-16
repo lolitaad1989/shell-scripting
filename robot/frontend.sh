0bc59ff82e86db101ded44e0b50626d82a062ad4
lolitaad989 <lolitaad19@gmail.com>
2023-01-16 20:04:39 +0530

    robot

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 
 ID=$(id -u)
-
-if [ $ID -ne 0 ] ; then
-    echo -e "\e[31m You need to run the script has root user \e[0m"
-    exit 1
-fi 
-
-echo -n "Installing Nginx : "
-yum install nginx -y  &>> /tmp/frontend.log
-if [ $? -eq 0 ] ; then
+COMPONENT=frontend
+ stat () {
+if [ $1 -eq 0 ] ; then
     echo -e "\e[32m Success \e[0m"
 else 
     echo -e "\e[31m Failure \e[0m"
 fi
+ }
+
+
+
+echo -n "Installing Nginx : "
+yum install nginx -y  &>> /tmp/frontend.log
+stat $?
 
 echo -n "Enabling Nginx : "
 systemctl enable nginx  &>> /tmp/frontend.log
-if [ $? -eq 0 ] ; then
-    echo -e "Success"
-else 
-    echo -e "Failure"
-fi
+stat $?
 
 echo -n "Starting Nginx : "
 systemctl start nginx  &>> /tmp/frontend.log
-if [ $? -eq 0 ] ; then
-    echo -e "Success"
-else 
-    echo -e "Failure"
-fi
+stat $?
+
+echo -n "Downloading the $COMPONENT : "
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+stat $?
+
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
