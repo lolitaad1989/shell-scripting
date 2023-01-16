fdd75435048d86ec2af9df0e3333494a64b1cad7
lolitaad989 <lolitaad19@gmail.com>
2023-01-16 18:19:53 +0530

    If

--- 13-loops.sh
+++ 13-loops.sh
@@ -3,3 +3,10 @@
 for courses in devops aws gcp azure terraform ansible docker ; do
     echo -e "Course is $courses \n"
 done
+
+val=0
+
+while [ $val -lt 10] ; do
+    echo "The value is $val"
+    val=$(($val+1))
+done
