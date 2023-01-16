17c94527029499580c97a16b5829fe594695b538
lolitaad989 <lolitaad19@gmail.com>
2023-01-16 11:08:40 +0530

    colors 3 v1

--- 03-colors.sh
+++ 03-colors.sh
@@ -6,3 +6,5 @@ echo -e "\e[31m I am printing Red Color \e[0m"
 echo -e "\e[33m I am printing Yellow Color \e[0m"
 echo -e "\e[34m I am printing Blue Color \e[0m"
 echo -e "\e[35m I am printing Cyan Color \e[0m"
+
+echo -e "\e[41;35m I am printing Cyan Color \e[0m"
