2cae7f45bc02051ac4be713a8f400a2cbe4c716c
lolitaad989 <lolitaad19@gmail.com>
2023-01-16 11:14:39 +0530

    variable3 v1

---
+++ 04-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+# What is variable ?
+# A variable is somehting which holds the value or data which would keep on changing as per the situation and scenario's
+# Typically we are telling the computer /program that uses this variable when running the scripts
+
+a=10 
+b=20
+c=30 
+d=100
+# JUNK_FOLDER_NAME=xyz
+echo a 
+echo $a 
