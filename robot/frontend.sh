4ed599eb7a433e6a8e1325c0ab463c925d14fff8
lolitaad989 <lolitaad19@gmail.com>
2023-01-16 19:55:51 +0530

    robot

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -6,3 +6,27 @@ if [ $ID -ne 0 ] ; then
     echo -e "\e[31m You need to run the script has root user \e[0m"
     exit 1
 fi 
+
+echo -n "Installing Nginx :"
+yum install nginx -y  &>> /tmp/frontend.log
+if [ $? -eq 0 ] ; then
+    echo -e "Success"
+else 
+    echo -e "Failure"
+    fi
+
+echo -n "Enabling Nginx :"
+systemctl enable nginx  &>> /tmp/frontend.log
+if [ $? -eq 0 ] ; then
+    echo -e "Success"
+else 
+    echo -e "Failure"
+    fi
+
+echo -n "Starting Nginx :"
+systemctl start nginx  &>> /tmp/frontend.log
+if [ $? -eq 0 ] ; then
+    echo -e "Success"
+else 
+    echo -e "Failure"
+    fi
