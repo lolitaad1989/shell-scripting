acd840f562bb95a40ff4f2e5b368c60aacc73980
lolitaad989 <lolitaad19@gmail.com>
2023-01-16 12:57:33 +0530

    adding lines to dynamic vars

---
+++ 06-special-vars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+echo "The script name is $0"
+a=$1
+b=$2
+#echo "Enter the value of a : $a "
+#echo "Enter the value of b : $b "
+
+echo "The value of a is $a"
+echo "The value of b is $b"
