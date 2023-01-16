33d6a3f86dabd52ca970b58ceaa282fee3ea8322
lolitaad989 <lolitaad19@gmail.com>
2023-01-16 17:53:00 +0530

    If

--- 11-if-condition.sh
+++ 11-if-condition.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+<<COMMENT
 a=$1
 if  [ $a == ABC ] ; then
     echo "Both values are equal"
@@ -8,3 +8,14 @@ else
     echo "Both are not equal"
     exit 1
 fi 
+COMMENT 
+
+c=$1
+
+if [ $c -eq '10' ] ; then
+    echo "Value is 10"
+elif [ $c -eq '15']
+    echo "Value is 15"
+elif [ $c -eq '25']
+    echo "Value is 25"
+fi
