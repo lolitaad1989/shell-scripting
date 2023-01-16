9bb28c684e30fb5281ae05a5d81e488a83d17c46
lolitaad989 <lolitaad19@gmail.com>
2023-01-16 19:16:15 +0530

    robot

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -3,6 +3,6 @@
 ID=$(id -u)
 
 if [ $ID -ne 0 ] ; then
-    echo -e "[\e[31m You need to run the script has root user \e[0m"
+    echo -e "\e[31m You need to run the script has root user \e[0m"
     exit 1
 fi 
