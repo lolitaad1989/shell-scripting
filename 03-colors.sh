30c82ad817131ebcdb7cadd54c396983c8f2c54b
lolitaad989 <lolitaad19@gmail.com>
2023-01-16 11:02:20 +0530

    colors 3 v1

---
+++ 03-colors.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Hello World"
+
+echo -e "\e[32m This is green color \e[0m"
+echo -e "\e[31m I am printing Red Color \e[0m"
+echo -e "\e[33m I am printing Yellow Color \e[0m"
+echo -e "\e[34m I am printing Blue Color \e[0m"
+echo -e "\e[35m I am printing Cyan Color \e[0m"
