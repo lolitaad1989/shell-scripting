872ce38606c29ec04ace725b248f445a89f19f80
lolitaad989 <lolitaad19@gmail.com>
2023-01-16 12:45:53 +0530

    adding lines to dynamic vars

--- 05-dynamic-vars.sh
+++ 05-dynamic-vars.sh
@@ -2,3 +2,4 @@
 DATE=$(date +%F)
 echo -e "Todays date is $DATE"
 echo -e "Good morning todays date is \e[33m $DATE \e[0m"
+echo -e "The total number of opened sessions is \e[32m $(who | wc -l)  \e[0m"
