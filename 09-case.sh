2cc9c30a1f4cc4199553acb4ea3c13af8f94a7e9
lolitaad989 <lolitaad19@gmail.com>
2023-01-16 17:10:35 +0530

    Case

--- 09-case.sh
+++ 09-case.sh
@@ -5,18 +5,18 @@ ACTION=$1
 case $ACTION in
     start)
         echo "xyz service is starting"
-        exit 0
+       # exit 0
         ;;
     stop)
         echo "xyz service is stopping"
-        exit 0
+      #  exit 0
         ;;
     restart)
         echo "xyz service is restarting"
-        exit 0
+      #  exit 0
         ;;
     *)
-        echo "Valid commands are start stop or restart only"
-        exit 0
+        echo -e "\e[32m Valid commands are start stop or restart only \e[0m"
+      #  exit 1
         ;;
 esac
