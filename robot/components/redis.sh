6cae76f6004efb435364fcff1d9e1e454dd79d56
lolitaad989 <lolitaad19@gmail.com>
2023-01-17 18:50:22 +0530

    robot

--- robot/components/redis.sh
+++ robot/components/redis.sh
@@ -1,15 +1,14 @@
 #!/bin/bash 
 
 COMPONENT=redis 
-
 source components/common.sh    # Source loads a file and this file has all the common patterns.
 
 echo -n "Configuring the $COMPONENT repo  : "
-curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>> "${LOFGILE}"
+curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>> "${LOGFILE}"
 stat $?
 
 echo -n "Intsalling $COMPONENT :"
-yum install redis-6.2.7 -y &>> "${LOFGILE}"
+yum install redis-6.2.7 -y &>> "${LOGFILE}"
 stat $? 
 
 echo -n "whitelisting the $COMPONENT"
@@ -19,6 +18,6 @@ stat $?
 
 echo -n "Starting $COMPONENT : "
 systemctl daemon-reload 
-systemctl enable $COMPONENT &>> "${LOFGILE}"
-systemctl restart  $COMPONENT &>> "${LOFGILE}"
+systemctl enable $COMPONENT &>> "${LOGFILE}"
+systemctl restart  $COMPONENT &>> "${LOGFILE}"
 stat $? 
