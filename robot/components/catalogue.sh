b77bb2c0e364d01da45db49c8de3fb5574744162
lolitaad989 <lolitaad19@gmail.com>
2023-01-17 15:09:53 +0530

    robot

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -10,10 +10,12 @@ curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>>
 yum install nodejs -y  &>> "${LOGFILE}"
 stat $?
 
-
-echo -n "Creating Application user $APPUSER"
-useradd roboshop &>> "${LOGFILE}"
-stat $?
+id $APPUSER &>> "${LOGFILE}"
+if [ $? -ne 0 ] ; then
+    echo -n "Creating Application user $APPUSER"
+    useradd roboshop &>> "${LOGFILE}"
+    stat $?
+fi
 
 echo -n "Downloading the $COMPONENT :"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
@@ -22,7 +24,11 @@ stat $?
 echo -n "Extracting the $COMPONENT :"
 cd /home/$APPUSER
 unzip -o /tmp/$COMPONENT.zip    &>> "${LOGFILE}"
+stat $?
+
+echo -n "Changing the permissions to $APPUSER"
 mv $COMPONENT-main $COMPONENT
+chown -R $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT
 cd /home/roboshop/$COMPONENT
 npm install
 stat $?
