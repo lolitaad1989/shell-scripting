975f29e0b370978f6ebf08f77df610a4378d9220
lolitaad989 <lolitaad19@gmail.com>
2023-01-17 13:20:30 +0530

    robot

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -12,11 +12,11 @@ echo -n "Downloading the $COMPONENT : "
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
 
 echo -n "Installing the $COMPONENT : "
-yum install -y mongodb-org  &>> $LOGFILE
+yum install -y mongodb-org  &>> $"${LOGFILE}"
 
 echo -n "Starting $COMPONENT : "
-systemctl enable mongod  &>> $LOGFILE
-systemctl start mongod   &>> $LOGFILE
+systemctl enable mongod  &>> $"${LOGFILE}"
+systemctl start mongod   &>> $"${LOGFILE}"
 stat $?
 
 echo -n "Downloading the $COMPONENT schema: "
@@ -25,11 +25,11 @@ stat $?
 
 echo -n "Extracting  $COMPONENT schema: "
 cd /tmp
-unzip -o  mongodb.zip   &>> $LOGFILE
+unzip -o  mongodb.zip   &>> $"${LOGFILE}"
 stat $?
 
 echo -n "Injecting  $COMPONENT schema: "
 cd mongodb-main   
-mongo < catalogue.js  &>> $LOGFILE
-mongo < users.js   &>> $LOGFILE
+mongo < catalogue.js  &>> $"${LOGFILE}"
+mongo < users.js   &>> $"${LOGFILE}"
 stat $?
