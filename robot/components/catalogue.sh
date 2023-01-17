1192d80e85ee2b316f3898e7f0fdaae8b769fadd
lolitaad989 <lolitaad19@gmail.com>
2023-01-17 15:13:45 +0530

    robot

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -21,14 +21,17 @@ echo -n "Downloading the $COMPONENT :"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
 stat $?
 
-echo -n "Extracting the $COMPONENT :"
+
+echo -n "Performing cleanup and extraction $COMPONENT :"
+rm -rf /home/$APPUSER/$COMPONENT/
 cd /home/$APPUSER
 unzip -o /tmp/$COMPONENT.zip    &>> "${LOGFILE}"
 stat $?
 
-echo -n "Changing the permissions to $APPUSER"
+
+echo -n "Changing the permissions to $APPUSER :"
 mv $COMPONENT-main $COMPONENT
 chown -R $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT
-cd /home/roboshop/$COMPONENT
-npm install
+# cd /home/roboshop/$COMPONENT
+# npm install
 stat $?
