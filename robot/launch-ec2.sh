b6a58ac85310001dd5ae4e14854098e2a57975f4
lolitaad989 <lolitaad19@gmail.com>
2023-01-18 20:42:12 +0530

    robot

--- robot/launch-ec2.sh
+++ robot/launch-ec2.sh
@@ -1,2 +1,10 @@
 #!/bin/bash 
 # This script creates the server and the DNS Record
+
+COMPONNET=$1
+AMIID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7" --region us-east-1 | jq .Images[].ImageId | sed -e 's/"//g')
+SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b52-allow-all --region=us-east-1 | jq .SecurityGroups[].GroupId| sed -e 's/"//g')
+
+echo "AMI ID used to launch hte instnace is $AMI_ID "
+echo "Security Group ID used to launch hte instnace is $SG_ID "
+echo "********** _________ $COMPONENT launch is in progress ______*********** "
